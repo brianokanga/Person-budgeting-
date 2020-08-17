479e5c61a0d6f9b312547d7bdc346ac5b000fbfb
Brian Okanga <briannokanga@gmail.com>
2020-08-18 01:42:28 +0300

    Added display updated budget

--- app.js
+++ app.js
@@ -107,6 +107,10 @@ let UIController = (function () {
 		inputBtn: '.add__btn',
 		incomeContainer: '.income__list',
 		expensesContainer: '.expenses__list',
+		budgetLabel: '.budget__value',
+		incomeLabel: '.budget__income--value',
+		expensesLabel: '.budget__expenses--value',
+		percentageLabel: '.budget__expenses--percentage',
 	};
 
 	return {
@@ -157,6 +161,22 @@ let UIController = (function () {
 			return fieldsArr;
 		},
 
+		displayBudget: function (obj) {
+			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+			document.querySelector(DOMstrings.expensesLabel).textContent =
+				obj.totalExp;
+			document.querySelector(DOMstrings.percentageLabel).textContent =
+				obj.percentage;
+
+			if (obj.percentage > 0) {
+				document.querySelector(DOMstrings.percentageLabel).textContent =
+					obj.percentage;
+			} else {
+				document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+			}
+		},
+
 		getDOMstrings: function () {
 			return DOMstrings;
 		},
@@ -187,7 +207,7 @@ let controller = (function (budgetCtrl, UICtrl) {
 		let budget = budgetCtrl.getBudget();
 
 		//3. Display the budget on thess UI
-		console.log(budget);
+		UICtrl.displayBudget(budget);
 	};
 
 	let ctrlAddItem = function () {
@@ -214,6 +234,12 @@ let controller = (function (budgetCtrl, UICtrl) {
 	return {
 		init: function () {
 			console.log('App has started');
+			UICtrl.displayBudget({
+				budget: 0,
+				totalInc: 0,
+				totalExp: 0,
+				percentage: -1,
+			});
 			setupEventListeners();
 		},
 	};
